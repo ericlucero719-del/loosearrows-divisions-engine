74e805c3828ddda3ec1ea2728bafeb70c0193101
ericlucero719-del <ericlucero719@gmail.com>
2026-03-27 13:03:36 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { division1Routes } from './divisions/division1';
 import { dispatchRoutes } from './divisions/division3-dispatch';
 import express from "express";
 import division2Router from "./routes/division2";
@@ -5,7 +6,7 @@ import dashboardRouter from "./routes/dashboard";
 
 export const app = express();
 const port = process.env.PORT || 3000;
-
+app.use('/division1', division1Routes);
 app.use(express.json());
 app.use("/division2", division2Router);
 app.use("/dashboard", dashboardRouter);
