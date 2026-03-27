14435c195c97ae35b98266a598a9ac276e96588c
ericlucero719-del <ericlucero719@gmail.com>
2026-03-27 02:57:52 -0600

    Update division3.routes.ts

--- src/division3/division3.routes.ts
+++ src/division3/division3.routes.ts
@@ -1,1 +1,10 @@
+import { Router } from 'express';
+import { division3Controller } from './division3.controller';
 
+const router = Router();
+
+router.post('/division3/process', (req, res) => {
+  division3Controller.handle(req, res);
+});
+
+export default router;
