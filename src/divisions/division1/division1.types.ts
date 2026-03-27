a2dfe67591c231f8beac38ed3838ad261fafe53e
ericlucero719-del <ericlucero719@gmail.com>
2026-03-27 12:43:18 -0600

    Update division1.types.ts

--- src/divisions/division1/division1.types.ts
+++ src/divisions/division1/division1.types.ts
@@ -1,1 +1,23 @@
+export interface ClinRecord {
+  clin: string;
+  sku: string;
+  description: string;
+  unitPrice: number;
+  uom: string;
+}
 
+export interface PriceQuote {
+  clin: string;
+  sku: string;
+  quantity: number;
+  unitPrice: number;
+  extendedPrice: number;
+  description: string;
+  uom: string;
+}
+
+export interface Division1TaskInput {
+  clin: string;
+  sku: string;
+  quantity: number;
+}
